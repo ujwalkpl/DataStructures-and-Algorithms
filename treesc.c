3ec9de0120313e5bf5bd26b505de8ee916093882
Ujwalkpl <ujwlkpl@gmail.com>
2018-12-08 19:41:11 +0530

    ewnjkrhfjk

--- treesc.c
+++ treesc.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void int main(int argc, char const *argv[]) {
   printf("Hello world\n", );
-  printf("are hou dsfj\n", );
+  printf("ardfnksadfe hou dsfj\n", );
   printf("kdfjkdjf\n", );
   return 0;
 }
